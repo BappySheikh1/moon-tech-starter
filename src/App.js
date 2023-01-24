7502b876df7e719a8240ccbf271a1429aa4e2f21
Your Name Main <you@example.com>
2023-01-24 09:29:35 -0800

    Product Cad added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { createContext } from "react";
+import React from "react";
 import { RouterProvider } from "react-router-dom";
+import ProductsProvider from "./Context/ProductsProvider";
 import routes from "./routes/routes";
 
-const Product_Context =createContext();
+
 
 function App() {
- const [products, setProducts]=useState([])
-  useEffect(()=>{
-   fetch('products.json')
-   .then(res => res.json())
-   .then(data => setProducts(data))
-  },[])
 
-  console.log(products);
   return (
     <div>
+      <ProductsProvider>
       <RouterProvider router={routes} />
+      </ProductsProvider>
     </div>
   );
 }
