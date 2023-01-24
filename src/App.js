9acd87969980bb408e08e0096fe3472b86dee6a6
Your Name Main <you@example.com>
2023-01-24 08:30:08 -0800

    conteext adde

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { createContext } from "react";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 
+const Product_Context =createContext();
+
 function App() {
+ const [products, setProducts]=useState([])
+  useEffect(()=>{
+   fetch('products.json')
+   .then(res => res.json())
+   .then(data => setProducts(data))
+  },[])
+
+  console.log(products);
   return (
     <div>
       <RouterProvider router={routes} />
